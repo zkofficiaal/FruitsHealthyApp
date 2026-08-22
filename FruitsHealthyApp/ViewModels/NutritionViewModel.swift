6c79665b55fc7d38dcba7adb63d919992969e04d
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-08-21 23:17:50 -0700

    docs(viewmodels): add MARK purpose comments to Home, Fruits, and Nutrition ViewModels

--- FruitsHealthyApp/ViewModels/NutritionViewModel.swift
+++ FruitsHealthyApp/ViewModels/NutritionViewModel.swift
@@ -5,3 +5,14 @@
 //  Created by Z.K   on 21/08/2026.
 //
 
+import Foundation
+import Combine
+
+// MARK: - Nutrition ViewModel
+final class NutritionViewModel: ObservableObject {
+    @Published var nutrition: DailyNutrition = .sample
+    // Holds daily nutrition data (sample by default)
+
+    @Published var selectedDate: String = "Today, 20 May"
+    // Tracks currently selected date for nutrition stats
+}
