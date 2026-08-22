7e4dd2ac899b6be56b23b75d26a42917cb443e8a
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-08-21 23:14:54 -0700

    feat(viewmodels): add FruitsViewModel with category filtering

---
+++ FruitsHealthyApp/ViewModels/HomeViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewModel.swift
+//  FruitsHealthyApp
+//
+//  Created by Z.K   on 21/08/2026.
+//
+
+import Foundation
+import Combine
+
+// MARK: - Home ViewModel
+final class HomeViewModel: ObservableObject {
+    @Published var userName: String = "DevTechZahid"
+    // Stores current user’s name for greeting
+
+    @Published var searchText: String = ""
+    // Tracks search input for filtering fruits
+
+    @Published var nutrition: DailyNutrition = .sample
+    // Holds daily nutrition data (sample by default)
+
+    @Published var topFruits: [Fruit] = Array(Fruit.sample.prefix(4))
+    // Displays top 4 fruits on home screen
+}
